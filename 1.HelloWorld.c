f43e70bad0b7c77f774a4dc0a81b2664e43f6343
unknown <thuhaytharzaw2021@gmail.com>
2023-03-17 18:43:06 +0630

    c and c++

---
+++ 1.HelloWorld.c
@@ -0,0 +1,13 @@
+#include<stdio.h> //standard input output
+#include<conio.h> //console input output
+
+int main(){
+    int age = 0;
+    printf("Please enter your name;");
+    scanf("%d",&age);
+    printf("My age is %d",age);
+
+
+    getch(); // get character
+return 0;
+}
