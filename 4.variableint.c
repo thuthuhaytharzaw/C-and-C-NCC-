6c648fca2e94673ab522ec3a302fbd77b3b89fd5
unknown <thuhaytharzaw2021@gmail.com>
2023-03-20 18:35:30 +0630

    c and c++

---
+++ 4.variableint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    int a=0;
+    int b=0;
+    int c=0;
+    int total=0;
+
+    printf("Please enter a number:");
+    scanf("%d\n",&a);
+    printf("Please enter second number:");
+    scanf("%d\n",&b);
+    printf("Please enter third number");
+    scanf("%d\n",&c);
+    total=a+b+c;
+    printf("This is total number:%d");
+    return 0;
+
+}
